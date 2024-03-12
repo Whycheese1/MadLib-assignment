bd4a7fcdf1b2411cce51cd57922393d2be99b7df
Whycheese1 <160049443+Whycheese1@users.noreply.github.com>
2024-03-12 11:04:30 -0600

    Add files via upload

---
+++ MadLib Assignment.py
@@ -0,0 +1,15 @@
+######################
+#MadLib Assignment
+#Jarrett Sanderson
+#March 12 2024
+#
+#Focus: EasyGUI, F-String, Formatting, Lists
+#
+#   Step 1: Create a simple Madlib which takes info from the user to create a short response. (10-15)
+#
+#   Step 2: Create a MadLib which asks for multiple values from user, and uses random input to create the final response.
+#######################
+
+import easygui as easy
+
+userQuestions = ["proper
